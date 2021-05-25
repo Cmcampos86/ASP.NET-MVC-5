fc9f97b44d88f56a16ca8af5f89a5c348cf8fee9
Claudio Marcos de Campos <cmcampos86@gmail.com>
2021-05-25 08:37:39 -0300

    Estudos MVC Bundle e Minification

--- TesteMVC5/TesteMVC5/App_Start/BundleConfig.cs
+++ TesteMVC5/TesteMVC5/App_Start/BundleConfig.cs
@@ -8,6 +8,25 @@ namespace TesteMVC5
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //bundles: pasta virtual
+            //Include: inclui todos os scripts em um só
+            //Pode ser usado máscara (asterísco)
+            //O bundles serve para minificar também
+            //Pasta Content é tudo em um só e os bundles são os separados minificados desde que esteja ativado esse recurso
+            //Quando for fazer o bundles, deve-se ter certeza que se pode misturar os arquivos, pois, pode acontecer de ter no javascript dois métodos com o mesmo nome e ao aplicar o bundles e o minification,
+                //voce não saberá qual deles está sendo chamado por questão com o mesmo nome
+
+            //habilita a otimização
+            //Com a minificação ativada, o nome de algumas variáveis podem ser alteradas para que o arquivo fique ainda menor
+            //Comentários são retirados
+            //A desvantagem se dá ao fato de se depurar um código que está na mesma linha
+
+            BundleTable.EnableOptimizations = true;
+
+            bundles.Add(new ScriptBundle("~/bundles/testes").Include(
+                        "~/Scripts/teste1.js",
+                        "~/Scripts/teste2.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
