c7286c135dcf9a21f7fcf6f787faa5a5fa195c6c
Claudio Marcos de Campos <cmcampos86@gmail.com>
2021-06-04 10:06:33 -0300

    MeusProdutos App - MVC

---
+++ MeusProdutos/MeusProdutos/src/DevIO.Business/Model/Fornecedores/Fornecedor.cs
@@ -0,0 +1,18 @@
+﻿using DevIO.Business.Core.Models;
+using DevIO.Business.Model.Produtos;
+using System.Collections.Generic;
+
+namespace DevIO.Business.Model.Fornecedores
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
