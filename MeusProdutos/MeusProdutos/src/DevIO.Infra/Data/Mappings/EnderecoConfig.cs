4500711e324705ae244f65a3b31f1aa3e8d85ece
Claudio Marcos de Campos <cmcampos86@gmail.com>
2021-06-04 17:20:15 -0300

    MeusProdutos App - MVC

---
+++ MeusProdutos/MeusProdutos/src/DevIO.Infra/Data/Mappings/EnderecoConfig.cs
@@ -0,0 +1,41 @@
+﻿using DevIO.Business.Model.Fornecedores;
+using System.Data.Entity.ModelConfiguration;
+
+namespace DevIO.Infra.Data.Mappings
+{
+    public class EnderecoConfig : EntityTypeConfiguration<Endereco>
+    {
+        public EnderecoConfig()
+        {
+            HasKey(p => p.Id);
+
+            Property(c => c.Logradouro)
+                //.HasColumnName("Rua") //Se o banco já existir e o nome da coluna for outro, para o banco vai "Rua" e para usar a propriedade vai "Logradouro"
+                .IsRequired()
+                .HasMaxLength(200);
+
+            Property(c => c.Numero)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            Property(c => c.Cep)
+                .IsRequired()
+                .HasMaxLength(8)
+                .IsFixedLength();//Campo fixo
+
+            Property(c => c.Complemento)
+                .HasMaxLength(250);
+
+            Property(c => c.Bairro)
+                .IsRequired();
+
+            Property(c => c.Cidade)
+                .IsRequired();
+
+            Property(c => c.Estado)
+                .IsRequired();
+
+            ToTable("Enderecos");
+        }
+    }
+}
---
+++ MeusProdutos/MeusProdutos/src/DevIO.Infra/Data/Mappings/FornecedorConfig.cs
@@ -0,0 +1,31 @@
+﻿using DevIO.Business.Model.Fornecedores;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+
+namespace DevIO.Infra.Data.Mappings
+{
+    public class FornecedorConfig : EntityTypeConfiguration<Fornecedor>
+    {
+        public FornecedorConfig()
+        {
+            //Mapeamento
+            HasKey(f => f.Id); //Chave Primária. É sempro bom informar, mas se não informar, o EF reconheceria pelas convenções
+            
+            Property(f => f.Nome)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            Property(f => f.Documento)
+                .IsRequired()
+                .HasMaxLength(14)
+                .HasColumnAnnotation("IX_Documento", 
+                    new IndexAnnotation(new IndexAttribute { IsUnique = true })); //Cria um índice
+
+            HasRequired(f => f.Endereco)
+                .WithRequiredPrincipal(e => e.Fornecedor); //Cria o relacionamento da tabela 1 X 1
+
+            ToTable("Fornecedores");
+        }
+    }
+}
