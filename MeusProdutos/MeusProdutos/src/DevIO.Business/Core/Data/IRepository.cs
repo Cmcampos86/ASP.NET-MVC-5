fc792d6584bc8f483a4068bba2cba842ff52ffba
Claudio Marcos de Campos <cmcampos86@gmail.com>
2021-06-04 10:24:21 -0300

    MeusProdutos App - MVC

---
+++ MeusProdutos/MeusProdutos/src/DevIO.Business/Core/Data/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using DevIO.Business.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Core.Data
+{
+    //Só será aceito as classes que herdarem de Entity
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        //Sempre trabalhar com assíncrono
+        Task Adicionar(TEntity entity);
+        Task<TEntity> ObterPorId(Guid id);
+        Task<List<TEntity>> ObterTodos();
+        Task Atualizar(TEntity entity);
+        Task Remover(Guid id);
+        Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate);
+        Task<int> SaveChanges();
+    }
+}
