1abfa30de5bb56e4896c3a7fa10536e231247ac6
Claudio Marcos de Campos <cmcampos86@gmail.com>
2021-06-09 08:42:54 -0300

    MeusProdutos App - MVC

---
+++ MeusProdutos/MeusProdutos/src/DevIO.AppMvc/Controllers/FornecedoresController.cs
@@ -0,0 +1,47 @@
+﻿using DevIO.Business.Model.Fornecedores;
+using DevIO.Business.Model.Fornecedores.Services;
+using DevIO.Infra.Data.Repository;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DevIO.AppMvc.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        #region Teste
+        private readonly IFornecedorService _fornecedorService;
+
+        public FornecedoresController()
+        {
+            _fornecedorService = new FornecedorService(new FornecedorRepository(), new EnderecoRepository());
+        }
+
+        // GET: Fornecedores
+        public async Task<ActionResult> Index()
+        {
+            var fornecedor = new Fornecedor()
+            {
+                Nome = "Claudio",
+                Documento = "35654022881",
+                Endereco = new Endereco()
+                {
+                    Logradouro = "Rua Teste",
+                    Numero = "123",
+                    Complemento = "Teste",
+                    Bairro = "Teste",
+                    Cep = "12345678",
+                    Cidade = "Teste",
+                    Estado = "Teste"
+                },
+                TipoFornecedor = TipoFornecedor.PessoaFisica,
+                Ativo = true
+            };
+
+            await _fornecedorService.Adicionar(fornecedor);
+
+            return new EmptyResult();
+        }
+
+        #endregion
+    }
+}
