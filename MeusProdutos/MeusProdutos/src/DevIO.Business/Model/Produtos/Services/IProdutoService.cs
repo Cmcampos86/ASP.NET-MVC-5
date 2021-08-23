35f37f7c2aa3e6b43a6045795690d0c181e3921c
Claudio Marcos de Campos <cmcampos86@gmail.com>
2021-08-23 08:21:25 -0300

    MeusProdutos App - MVC

---
+++ MeusProdutos/MeusProdutos/src/DevIO.Business/Model/Produtos/Services/IProdutoService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Model.Produtos.Services
+{
+    public interface IProdutoService :IDisposable
+    {
+        Task Adicionar(Produto produto);
+        Task Atualizar(Produto produto);
+        Task Remover(Guid id);
+    }
+}
