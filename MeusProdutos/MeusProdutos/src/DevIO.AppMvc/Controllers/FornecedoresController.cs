56ebc42ffdb244af582d1087c8cba49d85ae2119
Claudio Marcos de Campos <cmcampos86@gmail.com>
2021-08-23 08:02:06 -0300

    MeusProdutos App - MVC

--- MeusProdutos/MeusProdutos/src/DevIO.AppMvc/Controllers/FornecedoresController.cs
+++ MeusProdutos/MeusProdutos/src/DevIO.AppMvc/Controllers/FornecedoresController.cs
@@ -1,46 +1,43 @@
-﻿using DevIO.Business.Model.Fornecedores;
-using DevIO.Business.Model.Fornecedores.Services;
-using DevIO.Infra.Data.Repository;
-using System.Threading.Tasks;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace DevIO.AppMvc.Controllers
 {
     public class FornecedoresController : Controller
     {
         #region Teste
-        private readonly IFornecedorService _fornecedorService;
 
-        public FornecedoresController()
-        {
-            _fornecedorService = new FornecedorService(new FornecedorRepository(), new EnderecoRepository());
-        }
+        //private readonly IFornecedorService _fornecedorService;
 
-        // GET: Fornecedores
-        public async Task<ActionResult> Index()
-        {
-            var fornecedor = new Fornecedor()
-            {
-                Nome = "Claudio",
-                Documento = "35654022881",
-                Endereco = new Endereco()
-                {
-                    Logradouro = "Rua Teste",
-                    Numero = "123",
-                    Complemento = "Teste",
-                    Bairro = "Teste",
-                    Cep = "12345678",
-                    Cidade = "Teste",
-                    Estado = "Teste"
-                },
-                TipoFornecedor = TipoFornecedor.PessoaFisica,
-                Ativo = true
-            };
+        //public FornecedoresController()
+        //{
+        //    _fornecedorService = new FornecedorService(new FornecedorRepository(), new EnderecoRepository());
+        //}
 
-            await _fornecedorService.Adicionar(fornecedor);
+        //// GET: Fornecedores
+        //public async Task<ActionResult> Index()
+        //{
+        //    var fornecedor = new Fornecedor()
+        //    {
+        //        Nome = "Claudio",
+        //        Documento = "35654022881",
+        //        Endereco = new Endereco()
+        //        {
+        //            Logradouro = "Rua Teste",
+        //            Numero = "123",
+        //            Complemento = "Teste",
+        //            Bairro = "Teste",
+        //            Cep = "12345678",
+        //            Cidade = "Teste",
+        //            Estado = "Teste"
+        //        },
+        //        TipoFornecedor = TipoFornecedor.PessoaFisica,
+        //        Ativo = true
+        //    };
 
-            return new EmptyResult();
-        }
+        //    await _fornecedorService.Adicionar(fornecedor);
+
+        //    return new EmptyResult();
+        //}
 
         #endregion
     }
