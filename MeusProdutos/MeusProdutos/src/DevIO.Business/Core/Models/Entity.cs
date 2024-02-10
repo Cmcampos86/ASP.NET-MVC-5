2dee79774105ed2de07c2846cc39a46efdddb9ab
Claudio Marcos de Campos <cmcampos86@gmail.com>
2024-02-10 19:36:11 -0300

    Estudos MVC5

--- MeusProdutos/MeusProdutos/src/DevIO.Business/Core/Models/Entity.cs
+++ MeusProdutos/MeusProdutos/src/DevIO.Business/Core/Models/Entity.cs
@@ -4,6 +4,7 @@ namespace DevIO.Business.Core.Models
 {
     public abstract class Entity
     {
+        //Classe base das Entidades
         protected Entity()
         {
             Id = Guid.NewGuid();
---
+++ MeusProdutos/MeusProdutos/src/DevIO.Infra/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace DevIO.Infra.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Data.Context.MeuDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(DevIO.Infra.Data.Context.MeuDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
