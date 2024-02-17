1149364def02d6ee793c4162731bee759bb6c42c
Claudio Marcos de Campos <cmcampos86@gmail.com>
2024-02-17 08:37:12 -0300

    Estudos MVC5

--- MeusProdutos/MeusProdutos/src/DevIO.AppMvc/ViewModels/ErrorViewModel.cs
+++ MeusProdutos/MeusProdutos/src/DevIO.AppMvc/ViewModels/ErrorViewModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace DevIO.AppMvc.ViewModels
 {
+    //Configuração de tratamento de erro personalizado está na web.config a partir da tag httpErrors
     public class ErrorViewModel
     {
         public int ErroCode { get; set; }
